e3263dcf4c2b9806bb5bb6c6b5c3224399cdc098
dreamvalidator <itscoolpravin@gmail.com>
2021-02-03 08:25:39 +0530

    new commit

--- weatherapi/src/main/java/com/weather/controller/TempreatureController.java
+++ weatherapi/src/main/java/com/weather/controller/TempreatureController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.weather.entity.Temprature;
 import com.weather.service.ClimateService;
 
+import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping("/")
@@ -20,6 +22,7 @@ public class TempreatureController {
 
 
     @GetMapping("/{lat}/{lon}")
+    @ApiOperation("Returns list of all weather data in the system.")
     public Temprature getTemprature(@PathVariable("lat") String lat,@PathVariable("lon") String lon)
     {
      return climateService.getTemprature(lat,lon);
